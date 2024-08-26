ada44305dc4633df096fb8e58b07120a8f2bcfd8
dylsteck <dylancsteck@gmail.com>
2024-08-25 22:56:35 -0400

    link to favicon.ico in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from 'geist/font/sans';
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Stecknologies",
@@ -14,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={GeistSans.className}>
         {children}
       </body>
